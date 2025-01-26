8f6aeb4214d6c88d754bedbf0be587cbf3795a34
Андрей Дорогин <makerkaper@gmail.com>
2025-01-27 01:37:24 +0500

    выплнена 4 задача 3 урока

---
+++ Task_03_04/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_03_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                var a = Console.ReadLine();
+                if(a == "exit")
+                {
+                    break;
+                }
+                if (a == "")
+                {
+                    break;
+                }
+                Console.Clear();
+            }
+        }
+    }
+}
