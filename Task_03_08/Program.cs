2a991ccc81abc1d2e94e58091e6ada03c8818af3
Андрей Дорогин <makerkaper@gmail.com>
2025-01-27 03:43:00 +0500

    выполнена 8 задача 3 урока

---
+++ Task_03_08/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_03_08
+{
+    internal class Program
+    {
+        /*Даны натуральные числа от 20 до 50. Напечатать те из них, которые делятся на 3, но не делятся на 5.
+         */
+        static void Main(string[] args)
+        {
+            int a = 20;
+            Console.WriteLine("Среди чисел от 20 до 50 делятся на 3, но не делятся на 5: ");
+
+            while(a < 50)
+            {
+                if (a % 3 == 0 && a %5 != 0)
+                {   
+                    Console.WriteLine(a);
+                }
+                a++;
+            }
+        }
+    }
+}
